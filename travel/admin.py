90402abd5b48251e2b5eddcb2fc2777d1a23f980
Ismar <ismar110501@gmail.com>
2023-07-16 16:01:21 +0600

    filters

--- travel/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from travel.models import TravelService, Hotel, News, Author
-
-admin.site.register(TravelService)
-admin.site.register(Hotel)
-admin.site.register(News)
-admin.site.register(Author)
