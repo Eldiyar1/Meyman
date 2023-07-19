dcb70387550d5e6ef46acccfba37915aa4241969
Eldiyar1 <mc.oks1ne@gmail.com>
2023-07-19 19:50:00 +0600

    eldiyar

--- apps/travel/urls.py
+++ apps/travel/urls.py
@@ -1,15 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from .views import PriceRangeViewSet, HousingTypeViewSet, AccommodationTypeViewSet, BedTypeViewSet, HotelViewSet, \
-    HostelViewSet, ApartmentViewSet, GuestHouseViewSet, TravelServiceViewSet, AuthorViewSet, NewsViewSet
+    ApartmentViewSet, HostelViewSet, GuestHouseViewSet, TravelServiceViewSet, AuthorViewSet, NewsViewSet
 
 router = DefaultRouter()
-router.register(r'price_ranges', PriceRangeViewSet,)
-router.register(r'housing_types', HousingTypeViewSet,)
-router.register(r'accommodation_types', AccommodationTypeViewSet,)
-router.register(r'bed_types', BedTypeViewSet,)
+router.register(r'price_ranges', PriceRangeViewSet, )
+router.register(r'housing_types', HousingTypeViewSet, )
+router.register(r'accommodation_types', AccommodationTypeViewSet, )
+router.register(r'bed_types', BedTypeViewSet, )
 router.register(r'hotels', HotelViewSet)
-router.register(r'apartment', ApartmentViewSet)
 router.register(r'hostels', HostelViewSet)
+router.register(r'apartment', ApartmentViewSet)
 router.register(r'guest-houses', GuestHouseViewSet)
 router.register(r'travel-services', TravelServiceViewSet)
 router.register(r'authors', AuthorViewSet)
