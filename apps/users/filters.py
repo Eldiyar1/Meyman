2a008a3973d09890af688e745356ae382d3a7407
Ismar <ismar110501@gmail.com>
2023-07-29 17:07:05 +0600

    filet

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -1,14 +1,12 @@
+
 import django_filters
 from .models import AccommodationReservation
-from .constants import BOOKING_CHOICES, PAYMENT_CHOICES
 
 class AccommodationReservationFilter(django_filters.FilterSet):
-    booking_type = django_filters.ChoiceFilter(choices=BOOKING_CHOICES)
-    payment_type = django_filters.ChoiceFilter(choices=PAYMENT_CHOICES)
-
     class Meta:
         model = AccommodationReservation
         fields = {
             'booking_type': ['exact'],
             'payment_type': ['exact'],
         }
+
