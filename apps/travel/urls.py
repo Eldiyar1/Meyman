78c2d7981fd3357f2ff3713cdfe9cad3d65b4a1f
Ismar <ismar110501@gmail.com>
2023-08-08 12:26:01 +0600

    favorite

--- apps/travel/urls.py
+++ apps/travel/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from .views import HotelViewSet, ApartmentViewSet, HostelViewSet, GuestHouseViewSet, SanatoriumViewSet, RatingViewSet, \
-    HouseReservationViewSet, RoomViewSet
+    HouseReservationViewSet, RoomViewSet, HouseFavoriteViewSet
 
 router = DefaultRouter()
 router.register('hotels', HotelViewSet)
@@ -11,5 +11,7 @@ router.register('sanatoriums', SanatoriumViewSet)
 router.register('house_reservations', HouseReservationViewSet)
 router.register('ratings', RatingViewSet)
 router.register('rooms', RoomViewSet)
+router.register('house_favorite', HouseFavoriteViewSet)
+
 
 urlpatterns = router.urls
