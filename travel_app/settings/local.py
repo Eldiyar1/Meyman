bde62f1667075bedd21baf414bf11c01a88af0f8
Ismar <ismar110501@gmail.com>
2023-08-10 16:13:47 +0600

    local

--- travel_app/settings/local.py
+++ travel_app/settings/local.py
@@ -4,6 +4,7 @@ from .development import *
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:3080",
+    "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:4040",
