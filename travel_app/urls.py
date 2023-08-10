0836b3a8cd8b4e03cc60a18a042a1fc5923b0c31
Eldiyar1 <mc.oks1ne@gmail.com>
2023-08-11 01:02:23 +0600

    fix_urls

--- travel_app/urls.py
+++ travel_app/urls.py
@@ -3,16 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .settings.yasg import urlpatterns_swagger as doc_urls
-from apps.travel.urls import router as travel_router
-from apps.news.urls import router as news_router
-from apps.travel_service.urls import router as travel_service_router
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.registry.extend(travel_router.registry)
-router.registry.extend(news_router.registry)
-router.registry.extend(travel_service_router.registry)
 
 urlpatterns = [
           path('admin/', admin.site.urls),
