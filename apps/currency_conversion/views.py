cf66d469564d7adfeb969220e93ee42af174d5c5
isko18 <toksonbaevislam2004@gmail.com>
2023-08-28 22:36:34 +0600

    currency

--- apps/currency_conversion/views.py
+++ apps/currency_conversion/views.py
@@ -2,15 +2,21 @@ from rest_framework import viewsets
 from .serializers import CurrencyConversionSerializer
 from .permissions import IsAdminUserOrReadOnly
 from .utils import CurrencyiewSet
+from rest_framework.response import Response
 
 
 
-class CurrencyConverterViewSet(viewsets.ModelViewSet):
+class CurrencyConverterViewSet(viewsets.ViewSet):
     serializer_class = CurrencyConversionSerializer
     permission_classes = [IsAdminUserOrReadOnly]
 
-    currency_view_set = CurrencyiewSet()  
+    currency_view_set = CurrencyiewSet()  # Создание экземпляра CurrencyiewSet
+
+    def get_queryset(self):
+        return None  # Возвращаем пустой QuerySet, чтобы избежать ошибки
+
+    def list(self, request):
+        return Response([])  # Возвращаем пустой список для метода list
 
     def create(self, request):
-        return self.currency_view_set.create(request)  
-
+        return self.currency_view_set.create(request) 
