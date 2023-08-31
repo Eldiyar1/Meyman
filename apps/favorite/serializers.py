91396a7a936cb48a686f1e2d289dc2bfab6b851f
Ismar <ismar110501@gmail.com>
2023-09-01 03:54:59 +0600

    countfavoriteandfixbug

--- apps/favorite/serializers.py
+++ apps/favorite/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from .models import WishlistAlbum, HouseFavorite
-from ..travel.serializers import HousingGetSerializer
 
 
 class HouseFavoriteSerializer(serializers.ModelSerializer):
-    housing = HousingGetSerializer(read_only=True)
-
-    def get_fields(self):
-        fields = super().get_fields()
-        fields['housing'] = HousingGetSerializer(read_only=True)
-        return fields
-
     class Meta:
         model = HouseFavorite
         fields = ('id',
@@ -19,8 +11,12 @@ class HouseFavoriteSerializer(serializers.ModelSerializer):
 
 class WishlistAlbumSerializer(serializers.ModelSerializer):
     houseFavorite = HouseFavoriteSerializer(many=True, read_only=True)
+    favorite_count = serializers.SerializerMethodField()
+
+    def get_favorite_count(self, obj):
+        return obj.houseFavorite.count()
 
     class Meta:
         model = WishlistAlbum
         fields = ('id',
-                  'user', 'title', 'houseFavorite',)
+                  'user', 'title', 'favorite_count', 'houseFavorite')
