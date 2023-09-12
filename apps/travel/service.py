e8fd7624d97330021255ceaf8efcf17390a4fb96
Ismar <ismar110501@gmail.com>
2023-09-12 17:40:11 +0600

    fix bug for image

--- apps/travel/service.py
+++ apps/travel/service.py
@@ -21,11 +21,14 @@ def get_average_rating(self, obj):
 
 def compress_image(self):
     from PIL import Image
-    img = Image.open(self.image.path)
-    if img != 'null' or img != "None":
-        img = img.convert('RGB')
-        img.thumbnail((800, 800))
-        img.save(self.image.path, 'JPEG', quality=90)
+    if self.image:
+        img = Image.open(self.image.path)
+        if img != 'null' or img != "None":
+            img = img.convert('RGB')
+            img.thumbnail((800, 800))
+            img.save(self.image.path, 'JPEG', quality=90)
+        else:
+            pass
     else:
         pass
 
